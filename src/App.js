56e699c7cc6f4f0126b24699ade83fe5a52f4a54
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-04-27 15:54:18 +0500

    fix

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Card } from 'react-bootstrap';
+import './app.css'
+function App() {
+
+  const [count, setCount] = useState(0)
+  if(count > 99){
+    setCount(0)
+  }
+  
+  
+  function name() {
+    if(window.confirm("Siz rostan ham ochirmoqchimisiz") == true){
+      setCount(0)
+    }
+  }
+
+
+
+
+  return (
+
+    <>
+      <Card className='fcard'>
+        <Card className='card1'>
+          <h1>{count}/99</h1>
+        </Card>
+      </Card>
+      <Card className='bcard'>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => name()}>Clear</button>
+      </Card>
+    </>
+  );
+}
+
+export default App;
